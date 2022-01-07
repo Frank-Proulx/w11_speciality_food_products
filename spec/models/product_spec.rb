cdae6654649f20315b111d695282df16930a06d0
Frank-Proulx <frankproulx7@gmail.com>
2022-01-07 15:43:18 -0800

    scope for most recent test passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,4 +10,35 @@ describe Product do
     product = Product.create({name: "the famous mario spaghetti", cost: 7, country_of_origin: "France"})
     expect(product.name()).to(eq("The Famous Mario Spaghetti"))
   end
+  
+  describe ('.three_most_recent') do
+    it("returns the three most recently added products") do
+      product1 = Product.create(name: "peppers", cost: 12, country_of_origin: "France")
+      product2 = Product.create(name: "grapes", cost: 13, country_of_origin: "Spain")
+      product3 = Product.create(name: "tomatoes", cost: 1, country_of_origin: "Japan")
+      product4 = Product.create(name: "lettuce", cost: 14, country_of_origin: "Russia")
+      product5 = Product.create(name: "peanut butter", cost: 7, country_of_origin: "Cameroon")
+      expect(Product.three_most_recent).to(eq([product5, product4, product3]))
+    end
+  end
+
+  # describe ('.most_reviews') do
+  #   it("returns the product with the most reviews") do
+  #     product1 = Product.create(name: "peppers", cost: 12, country_of_origin: "France")
+  #     product2 = Product.create(name: "grapes", cost: 13, country_of_origin: "Spain")
+  #     product3 = Product.create(name: "tomatoes", cost: 1, country_of_origin: "Japan")
+  #     review1 = Review.create(author: "mitch paper", content_body: "blah blah blah sjdk lkj ljasdf lkasdjflkj asdlkjasd sjdkjl aslkdjds", rating: 1, product_id: 1)
+  #     review2 = Review.create(author: "sally paper", content_body: "blah blah zffdg sjdk lkj ljasdf lkasdjflkj asdlkjasd sjdkjl aslkdjds", rating: 2, product_id: 1)
+  #     review3 = Review.create(author: "chet paper", content_body: "blah blah sdfg sjdk lkj ljasdf lkasdjflkj asdlkjasd sjdkjl aslkdjds", rating: 3, product_id: 1)
+  #     review4 = Review.create(author: "bill paper", content_body: "blah blah gfds sjdk lkj ljasdf lkasdjflkj asdlkjasd sjdkjl aslkdjds", rating: 4, product_id: 2)
+  #     review5 = Review.create(author: "steve paper", content_body: "blah blah hjjh sjdk lkj ljasdf lkasdjflkj asdlkjasd sjdkjl aslkdjds", rating: 5, product_id: 2)
+  #     expect(Product.most_reviews).to(eq(product1))
+  #   end
+  # end
+
+  # describe ('.usa') do
+  #   it("returns products with a country of origin matching USA") do
+  #   expect(Product.three_most_recent).to(eq())
+  #   end
+  # end
 end
