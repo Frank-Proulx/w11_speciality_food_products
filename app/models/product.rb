6372bc97dc3d9a0e054faad0c4f7e5b0076c7387
Frank-Proulx <frankproulx7@gmail.com>
2022-01-07 13:43:02 -0800

    add scopes for most reviews and newest products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,14 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   validates :cost, numericality: { only_integer: true }
   before_save(:titleize_product)
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3) }
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
 
   private
     def titleize_product
