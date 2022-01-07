5c83bef1528be1fc13c913b5fe82d4b2a06cb5fd
Frank-Proulx <frankproulx7@gmail.com>
2022-01-07 10:58:05 -0800

    add validations with passing tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates_length_of :content_body, minimum: 50
+  validates_length_of :content_body, maximum: 250
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 1 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
