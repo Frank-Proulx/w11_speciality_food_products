4c654049b5efbff6b7d6d07446832d87880559ad
Frank-Proulx <frankproulx7@gmail.com>
2022-01-14 12:39:25 -0800

    add integration testing for a non user trying to delete a product or review

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -53,3 +53,24 @@ describe "the delete a product process as an admin" do
     expect(page).to have_no_content 'Fancy Peppers'
   end
 end
+
+describe "the delete a product process as a non admin user" do
+  it "confirms a non user can't delete a product" do
+    user = User.create({email: 'example@email.com', password: 'testing', admin: true})
+    visit '/'
+    click_link 'Sign In'
+    fill_in 'Email', :with => 'example@email.com'
+    fill_in 'Password', :with => 'testing'
+    click_on 'Log in'
+    click_link 'Add new product'
+    fill_in 'Name', :with => 'Fancy Peppers'
+    fill_in 'Cost', :with => '5'
+    fill_in 'Country of origin', :with => 'France'
+    click_on 'Create Product'
+    click_on 'Sign Out'
+    click_link 'Fancy Peppers'
+    click_on 'Delete'
+    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
--- spec/features/delete_review_pages_spec.rb
+++ spec/features/delete_review_pages_spec.rb
@@ -58,3 +58,31 @@ describe "the delete a review process as a non admin user" do
     expect(page).to have_content 'Mike Tyson'
   end
 end
+
+describe "the delete a review process as a non user" do
+  it "confirms a non user can't delete a review" do
+    user = User.create({email: 'example@email.com', password: 'testing', admin: true})
+    visit '/'
+    click_link 'Sign In'
+    fill_in 'Email', :with => 'example@email.com'
+    fill_in 'Password', :with => 'testing'
+    click_on 'Log in'
+    click_link 'Add new product'
+    fill_in 'Name', :with => 'Fancy Peppers'
+    fill_in 'Cost', :with => '5'
+    fill_in 'Country of origin', :with => 'France'
+    click_on 'Create Product'
+    click_link 'Fancy Peppers'
+    click_link 'Add a review'
+    fill_in 'Author', :with => 'Mike Tyson'
+    fill_in 'Content body', :with => 'blalsjdf sdkjhl lkajshdf laksdj kljashdfl hasldkjfh alskdjfhalsdjkfh alskdjhflaksdjhf'
+    fill_in 'Rating', :with => '4'
+    click_on 'Create Review'
+    click_on 'Sign Out'
+    click_link 'Fancy Peppers'
+    click_link 'Mike Tyson'
+    click_on 'Delete review'
+    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
